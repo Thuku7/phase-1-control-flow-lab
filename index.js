b8f161c988405784bb38609368810e4904ea6bca
Robert <robethuku11@gmail.com>
2022-11-27 22:04:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result;
+  if(feet <= 400) {
+    result = "This one is on me!";
+
+  }else if(feet > 2500){
+    result = "No can do.";
+  }else if (feet > 2000) {
+    result = "I will gladly take your thirty bucks."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result = (city ==='NYC') ? 'Ok, sounds good.' : 'No go.'
+      return result;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result;
+  switch(tip) {
+    case 'generous':
+      result ='Thank you so much.';
+      break;
+    case 'not as generous':
+      result ='Thank you.';
+      break;  
+    case 'thanks for everything':
+      result ='Bye.';
+      break;    
+  }
+  return result;
+  
 }
+
+scuberGreetingForFeet(2501);
+ternaryCheckCity('Pittsburgh');
